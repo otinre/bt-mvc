cd099cabc40c948eae49a0db66f5e06d755ad562
Ernestas <otinre@gmail.com>
2018-10-31 16:40:36 +0200

    pridedu baigat darba

---
+++ core/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+// Core components paths
+define('CORE_DIR', 'core' . DIRECTORY_SEPARATOR);
+define('CORE_LIB_DIR', CORE_DIR . 'lib' . DIRECTORY_SEPARATOR);
+define('CORE_COMPONENTS_DIR', CORE_LIB_DIR . 'components' . DIRECTORY_SEPARATOR);
+// User code paths
+define('SRC_DIR', 'src' . DIRECTORY_SEPARATOR);
+define('MODEL_DIR', SRC_DIR . 'models' . DIRECTORY_SEPARATOR);
+define('CONTROLLER_DIR', SRC_DIR . 'controllers' . DIRECTORY_SEPARATOR);
+define('VIEW_DIR', SRC_DIR . 'views' . DIRECTORY_SEPARATOR);
+define('TEMPLATE_DIR', VIEW_DIR . 'templates'. DIRECTORY_SEPARATOR);
+// Main layout template file name base
+define('BASE_TEMPLATE', 'base');
+?>
--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-// Core components paths
-define('CORE_DIR', 'core' . DIRECTORY_SEPARATOR);
-define('CORE_LIB_DIR', CORE_DIR . 'lib' . DIRECTORY_SEPARATOR);
-define('CORE_COMPONENTS_DIR', CORE_LIB_DIR . 'components' . DIRECTORY_SEPARATOR);
-// User code paths
-define('SRC_DIR', 'src' . DIRECTORY_SEPARATOR);
-define('MODEL_DIR', SRC_DIR . 'models' . DIRECTORY_SEPARATOR);
-define('CONTROLLER_DIR', SRC_DIR . 'controllers' . DIRECTORY_SEPARATOR);
-define('VIEW_DIR', SRC_DIR . 'views' . DIRECTORY_SEPARATOR);
-define('TEMPLATE_DIR', VIEW_DIR . 'templates'. DIRECTORY_SEPARATOR);
-// Main layout template file name base
-define('BASE_TEMPLATE', 'base');
+if (file_exists('config.inc.php')) {
+    include_once 'config.inc.php';
+    } else {
+    die('Structure mismatch: config.inc.php');
+    }
 $indexName = basename(__FILE__);
 $baseDir = strtr(__DIR__, '/\\', DIRECTORY_SEPARATOR);
 $baseUrl = substr($_SERVER['SCRIPT_NAME'], 0, strpos($_SERVER['SCRIPT_NAME'], $indexName));
